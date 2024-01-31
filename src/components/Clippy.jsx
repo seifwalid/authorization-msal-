f7424c410923d8b2a2a221c19c04f685a9b9963f
Meedil <ali.haitham.ymy@gmail.com>
2024-01-31 12:15:52 +0200

    add button to hide clippy

--- src/components/Clippy.jsx
+++ src/components/Clippy.jsx
@@ -1,12 +1,19 @@
 import clippy_img from "../assets/clippy.png";
 import SpeechBubble from "./SpeechBubble";
 import "./Clippy.css";
+import { useState } from "react";
 
 export default function Clippy() {
+	const [hidden, setHidden] = useState(false);
+
+	const hideClippy = () => {
+		setHidden(true);
+	};
+
 	return (
 		<div className="clippy-container">
-			<div className="clippy">
-				<SpeechBubble>
+			<div className={`clippy ${hidden ? "hidden" : ""}`}>
+				<SpeechBubble hideBubble={() => hideClippy()}>
 					What am I supposed to write here, Seif? Am I supposed to write about
 					this app&apos;s functionality? or what exactly. please help.
 				</SpeechBubble>
--- src/components/SpeechBubble.jsx
+++ src/components/SpeechBubble.jsx
@@ -1,5 +1,12 @@
 import "./SpeechBubble.css";
 
 export default function SpeechBubble(props) {
-	return <div className={"speech-bubble"}>{props.children}</div>;
+	return (
+		<div className={"speech-bubble"}>
+			<button className="hide-bubble-btn" onClick={() => props.hideBubble()}>
+				✖
+			</button>
+			{props.children}
+		</div>
+	);
 }
